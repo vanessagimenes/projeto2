1e65660adf7c46cb027a849f23c73e74921dcc3b
vanessagimenes <109761131+vanessagimenes@users.noreply.github.com>
2022-08-21 12:16:33 -0300

    Create cadsql.sql

---
+++ cadsql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE  cadastro (
+    AlunoID int,
+    Nome varchar ( 30 ),
+    Sobrenome varchar ( 30 ),
+    Endereco varchar ( 90 ),
+    Cidade varchar ( 30 ),
+Varchar do     host ( 50 )
+);
